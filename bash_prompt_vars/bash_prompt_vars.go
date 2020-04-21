f7ed99c8d347f21c6bea398d796e6fc7fa1ef71f
Albert Hopkins <marduk@letterboxes.org>
2020-04-21 08:46:30 -0700

    bash_prompt_vars: add mercurial vcs support
    If the current directory is in a mercurial repo then will output vcs variable
    just like bash_prompt.sh.

--- bash_prompt_vars/bash_prompt_vars.go
+++ bash_prompt_vars/bash_prompt_vars.go
@@ -4,8 +4,10 @@ import (
 	"fmt"
 	"os"
 	"os/exec"
+	"path"
 	"strings"
 
+	hg "bitbucket.org/gohg/gohg"
 	"github.com/shirou/gopsutil/host"
 	"github.com/shirou/gopsutil/load"
 	"github.com/shirou/gopsutil/process"
@@ -15,6 +17,71 @@ func checkError(err error, msg string) {
 	if err != nil {
 		panic(fmt.Sprintf("%s: %s\n", msg, err))
 	}
+}
+
+func printError(err error) error {
+	if err != nil {
+		fmt.Println(err)
+	}
+
+	return err
+}
+
+func hgStatus() string {
+	hgcl := hg.NewHgClient()
+
+	err := hgcl.Connect("hg", ".", nil, false)
+
+	if err != nil {
+		return ""
+	}
+
+	defer hgcl.Disconnect()
+
+	root := path.Base(hgcl.RepoRoot())
+
+	id, err := hgcl.Identify([]hg.HgOption{}, []string{"-i", "-b"})
+
+	if printError(err) != nil {
+		return ""
+	}
+
+	split := strings.Split(strings.TrimRight(string(id), "\n"), " ")
+	revision := split[0][0:7]
+	branch := split[1]
+
+	status, err := hgcl.Status([]hg.HgOption{}, []string{})
+
+	if printError(err) != nil {
+		return ""
+	}
+
+	added := 0
+	deleted := 0
+	modified := 0
+	untracked := 0
+	var char string
+
+	lines := strings.Split(string(status), "\n")
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		switch char = string(line[0]); char {
+		case "M":
+			modified++
+		case "A":
+			added++
+		case "D":
+		case "R":
+			deleted++
+		default:
+			untracked++
+		}
+	}
+
+	return fmt.Sprintf("hg %s %s %s %dm %da %dd %du", root, branch, revision, modified, added, deleted, untracked)
 }
 
 func getTty() string {
@@ -63,10 +130,12 @@ func main() {
 	}
 
 	os = strings.Title(os)
+	vcs := hgStatus()
 
 	fmt.Printf("load=\"%.2f %.2f %.2f\"\n", loadavg.Load1, loadavg.Load5, loadavg.Load15)
 	fmt.Printf("myos=\"%s\"\n", os)
 	fmt.Printf("myversion=\"%s\"\n", version)
 	fmt.Printf("tty=\"%s\"\n", terminal)
 	fmt.Printf("users=\"%d\"\n", users)
+	fmt.Printf("vcs=\"%s\"\n", vcs)
 }
